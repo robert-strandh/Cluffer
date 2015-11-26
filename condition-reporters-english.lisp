767ee9b634a15ca1238276425185ac799b4e52a6
Robert Strandh <robert.strandh@gmail.com>
2015-11-26 07:59:31 +0100

    New condition reporter for condition END-OF-LINE.

--- condition-reporters-english.lisp
+++ condition-reporters-english.lisp
@@ -5,3 +5,9 @@
      stream
      (language acclimation:english))
   (format stream "Attempt to move beyond the beginning of a line."))
+
+(defmethod acclimation:report-condition
+    ((condition end-of-line)
+     stream
+     (language acclimation:english))
+  (format stream "Attempt to move beyond the end of a line."))
