a208e89fc656d0a2cf2dd7b73c5e45afe2cf7140
Robert Strandh <robert.strandh@gmail.com>
2015-12-05 07:15:41 +0100

    Add generic functions CURSOR-POSITION and (SETF CURSOR-POSITION).

--- edit-protocol.lisp
+++ edit-protocol.lisp
@@ -6,3 +6,19 @@
   ((%dock :initarg :dock :initform nil :accessor dock)))
 
 (defclass cursor () ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function CURSOR-POSITION.
+;;;
+;;; Given a cursor, return its conceptual position.
+
+(defgeneric cursor-position (cursor))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function (SETF CURSOR-POSITION).
+;;;
+;;; Given a cursor, set its conceptual position.
+
+(defgeneric (setf cursor-position) (position cursor))
