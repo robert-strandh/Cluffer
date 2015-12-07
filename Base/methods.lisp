573469a4f11b3c746977e94c87ae3ad0093714d6
Robert Strandh <robert.strandh@gmail.com>
2015-12-07 06:19:05 +0100

    Add default method on BEGINNING-OF-LINE-P.

--- Base/methods.lisp
+++ Base/methods.lisp
@@ -1,1 +1,10 @@
 (cl:in-package #:cluffer-base)
+
+;;; Default method on BEGINNING-OF-LINE-P.  
+;;;
+;;; Simple implementations of the line protocol can rely on the
+;;; existence of this method.  More sophisticated implementations
+;;; might use a more optimized way of determining whether the cursor
+;;; is at the beginning of the line
+(defmethod cluffer:beginning-of-line-p (cursor)
+  (zerop (cluffer:cursor-position cursor)))
