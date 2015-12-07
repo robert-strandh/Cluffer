7ffb2d0831e171e479d432eeb4c7470473840df7
Robert Strandh <robert.strandh@gmail.com>
2015-12-07 06:20:33 +0100

    Add default method on END-OF-LINE-P.

--- Base/methods.lisp
+++ Base/methods.lisp
@@ -5,6 +5,16 @@
 ;;; Simple implementations of the line protocol can rely on the
 ;;; existence of this method.  More sophisticated implementations
 ;;; might use a more optimized way of determining whether the cursor
-;;; is at the beginning of the line
+;;; is at the beginning of the line.
 (defmethod cluffer:beginning-of-line-p (cursor)
   (zerop (cluffer:cursor-position cursor)))
+
+;;; Default method on END-OF-LINE-P.  
+;;;
+;;; Simple implementations of the line protocol can rely on the
+;;; existence of this method.  More sophisticated implementations
+;;; might use a more optimized way of determining whether the cursor
+;;; is at the end of the line.
+(defmethod cluffer:end-of-line-p (cursor)
+  (= (cluffer:cursor-position cursor)
+     (cluffer:item-count cursor)))
