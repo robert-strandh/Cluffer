5a1cc5cb30f00d2d2c370694431c33b67825edde
Robert Strandh <robert.strandh@gmail.com>
2015-12-07 06:34:34 +0100

    Use explicit package prefixes to refer to symbols in the CLUFFER package.

--- Base/methods.lisp
+++ Base/methods.lisp
@@ -21,18 +21,18 @@
 
 ;;; This :BEFORE method checks whether the cursor is attached, and if
 ;;; not, signals an error.
-(defmethod cursor-position :before (cursor)
-  (unless (cursor-attached-p cursor)
+(defmethod (cluffer:cursor-position :before (cursor)
+  (unless (cluffer:cursor-attached-p cursor)
     (error 'cluffer:cursor-detached)))
 
 ;;; This :BEFORE method checks whether the cursor is attached, and if
 ;;; not, signals an error.
-(defmethod beginning-of-line-p :before (cursor)
-  (unless (cursor-attached-p cursor)
+(defmethod cluffer:beginning-of-line-p :before (cursor)
+  (unless (cluffer:cursor-attached-p cursor)
     (error 'cluffer:cursor-detached)))
 
 ;;; This :BEFORE method checks whether the cursor is attached, and if
 ;;; not, signals an error.
-(defmethod end-of-line-p :before (cursor)
-  (unless (cursor-attached-p cursor)
+(defmethod cluffer:end-of-line-p :before (cursor)
+  (unless (cluffer:cursor-attached-p cursor)
     (error 'cluffer:cursor-detached)))
