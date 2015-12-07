1f155492de9023f8c8233ebf6ab697fcaf2e7358
Robert Strandh <robert.strandh@gmail.com>
2015-12-07 06:13:08 +0100

    New component containing package definitions for protocol and base functions.

---
+++ Base/packages.lisp
@@ -0,0 +1,25 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cluffer
+  (:use #:common-lisp)
+  (:export
+   #:buffer
+   #:line
+   #:cursor
+   #:item-count
+   #:cursor-position
+   #:beginning-of-line-p
+   #:end-of-line-p
+   #:forward-item
+   #:backward-item
+   #:beginning-of-line
+   #:end-of-line
+   #:item-before-cursor
+   #:item-after-cursor
+   #:insert-item
+   #:delete-item
+   #:erase-item))
+
+(defpackage #:cluffer-base
+  (:use #:common-lisp)
+  (:export))
