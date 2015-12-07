ea0e26ad7d7adfef7814784d24535e6cdd36e49c
Robert Strandh <robert.strandh@gmail.com>
2015-12-07 07:08:29 +0100

    Turn method on END-OF-LINE into a :BEFORE method and move it to base system.

--- Base/methods.lisp
+++ Base/methods.lisp
@@ -61,3 +61,9 @@
 (defmethod cluffer:beginning-of-line :before (cursor)
   (unless (cluffer:cursor-attached-p cursor)
     (error 'cluffer:cursor-detached)))
+
+;;; This :BEFORE method checks whether the cursor is attached, and if
+;;; not, signals an error.
+(defmethod cluffer:end-of-line :before (cursor)
+  (unless (cluffer:cursor-attached-p cursor)
+    (error 'cluffer:cursor-detached)))
