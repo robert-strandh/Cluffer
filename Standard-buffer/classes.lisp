11fc70f99e7fc5b31eca78ec665218c3e3359fa0
Robert Strandh <robert.strandh@gmail.com>
2015-12-08 09:26:48 +0100

    Move class definitions to component dedicated for that purpose.

---
+++ Standard-buffer/classes.lisp
@@ -0,0 +1,21 @@
+(cl:in-package #:cluffer-standard-buffer)
+
+(defclass buffer ()
+  ((%current-time :initform 0 :initarg :current-time :accessor current-time)
+   (%contents :initarg :contents :accessor contents)))
+
+;;; The node contains a reference to the buffer in which it is
+;;; located.  This reference is needed because when a node of the tree
+;;; is splayed, that node must be explicitly assigned to the CONTENTS
+;;; slot of the buffer.
+(defclass node (clump-binary-tree:node-with-parent)
+  ((%buffer :initform nil :initarg :buffer :accessor buffer)
+   (;; The line count of the entire subtree.
+    %line-count :initarg :line-count :accessor line-count)
+   (;; The item count of the entire subtree.
+    %item-count :initarg :item-count :accessor item-count)
+   (%create-time :initarg :create-time :reader create-time)
+   (%modify-time :initarg :modify-time :accessor modify-time)
+   (%max-modify-time :initarg :max-modify-time :accessor max-modify-time)
+   (%line :initarg :line :accessor line)))
+
