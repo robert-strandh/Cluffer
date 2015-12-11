9afde4ca86d8b3ba6b9e7a70eb0cdbeaa8c3ef65
Robert Strandh <robert.strandh@gmail.com>
2015-12-11 09:00:09 +0100

    Add comments explaining what the tests do.

--- Test/test.lisp
+++ Test/test.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:cluffer-test)
 
+;;; Check that moving backward on an empty simple line signals an
+;;; error of type BEGINNING-OF-LINE.
 (defun test-simple-line-1 ()
   (let ((line (make-instance 'cluffer-simple-line:line))
 	(cursor (make-instance
@@ -8,6 +10,8 @@
     (assert (typep (nth-value 1 (ignore-errors (cluffer:backward-item cursor)))
 		   'cluffer:beginning-of-line))))
 
+;;; Check that moving forward on an empty simple line signals an error
+;;; of type END-OF-LINE.
 (defun test-simple-line-2 ()
   (let ((line (make-instance 'cluffer-simple-line:line))
 	(cursor (make-instance
