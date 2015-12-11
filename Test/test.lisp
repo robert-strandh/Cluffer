907c1455f7dcc0b5c0bc1e0e88680794d5f6e33e
Robert Strandh <robert.strandh@gmail.com>
2015-12-11 08:58:13 +0100

    Test that moving forward on an empty simple line signals the correct error.

--- Test/test.lisp
+++ Test/test.lisp
@@ -8,8 +8,17 @@
     (assert (typep (nth-value 1 (ignore-errors (cluffer:backward-item cursor)))
 		   'cluffer:beginning-of-line))))
 
+(defun test-simple-line-2 ()
+  (let ((line (make-instance 'cluffer-simple-line:line))
+	(cursor (make-instance
+		    'cluffer-simple-line:detached-right-sticky-cursor)))
+    (cluffer:attach-cursor cursor line)
+    (assert (typep (nth-value 1 (ignore-errors (cluffer:forward-item cursor)))
+		   'cluffer:end-of-line))))
+
 (defun test-simple-line ()
-  (test-simple-line-1))
+  (test-simple-line-1)
+  (test-simple-line-2))
 
 (defun run-tests ()
   (test-simple-line))
