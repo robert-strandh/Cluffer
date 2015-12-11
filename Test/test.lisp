067d256bf8415c0349ec175da8e1d705f143b69f
Robert Strandh <robert.strandh@gmail.com>
2015-12-11 09:45:22 +0100

    Test that ITEM-BEFORE-CURSOR signals an error of the correct type for an empty line.

--- Test/test.lisp
+++ Test/test.lisp
@@ -88,13 +88,32 @@
   (let ((line (make-instance 'cluffer-simple-line:line)))
     (assert (zerop (cluffer:item-count line)))))
 
+;;; Check that calling ITEM-BEFORE-CURSOR on an empty simple line
+;;; signals an error of type BEGINNING-OF-LINE.
+(defun test-simple-line-7 ()
+  ;;; Run the test for a left-sticky cursor.
+  (let ((line (make-instance 'cluffer-simple-line:line))
+	(cursor (make-instance
+		    'cluffer-simple-line:detached-left-sticky-cursor)))
+    (cluffer:attach-cursor cursor line)
+    (assert (typep (nth-value 1 (ignore-errors (cluffer:item-before-cursor cursor)))
+		   'cluffer:beginning-of-line)))
+  ;;; Run the test for a right-sticky cursor.
+  (let ((line (make-instance 'cluffer-simple-line:line))
+	(cursor (make-instance
+		    'cluffer-simple-line:detached-right-sticky-cursor)))
+    (cluffer:attach-cursor cursor line)
+    (assert (typep (nth-value 1 (ignore-errors (cluffer:item-before-cursor cursor)))
+		   'cluffer:beginning-of-line))))
+
 (defun test-simple-line ()
   (test-simple-line-1)
   (test-simple-line-2)
   (test-simple-line-3)
   (test-simple-line-4)
   (test-simple-line-5)
-  (test-simple-line-6))
+  (test-simple-line-6)
+  (test-simple-line-7))
 
 (defun run-tests ()
   (test-simple-line))
