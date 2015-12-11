67408bda7c06d03cfeb3618c45dba9f745159b4b
Robert Strandh <robert.strandh@gmail.com>
2015-12-11 09:08:31 +0100

    Test that the cursor position of a cursor in an empty line is 0.

--- Test/test.lisp
+++ Test/test.lisp
@@ -36,9 +36,25 @@
     (assert (typep (nth-value 1 (ignore-errors (cluffer:forward-item cursor)))
 		   'cluffer:end-of-line))))
 
+;;; Check that the cursor position of a cursor in an empty line is 0.
+(defun test-simple-line-3 ()
+  ;;; Run the test for a left-sticky cursor.
+  (let ((line (make-instance 'cluffer-simple-line:line))
+	(cursor (make-instance
+		    'cluffer-simple-line:detached-left-sticky-cursor)))
+    (cluffer:attach-cursor cursor line)
+    (assert (zerop (cluffer:cursor-position cursor))))
+  ;;; Run the test for a right-sticky cursor.
+  (let ((line (make-instance 'cluffer-simple-line:line))
+	(cursor (make-instance
+		    'cluffer-simple-line:detached-right-sticky-cursor)))
+    (cluffer:attach-cursor cursor line)
+    (assert (zerop (cluffer:cursor-position cursor)))))
+
 (defun test-simple-line ()
   (test-simple-line-1)
-  (test-simple-line-2))
+  (test-simple-line-2)
+  (test-simple-line-3))
 
 (defun run-tests ()
   (test-simple-line))
