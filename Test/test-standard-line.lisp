48b75f71b11bf9dafefcbf549df4beff34674609
Robert Strandh <robert.strandh@gmail.com>
2015-12-12 12:27:35 +0100

    Beginning of function for testing using random insertions.

--- Test/test-standard-line.lisp
+++ Test/test-standard-line.lisp
@@ -3,3 +3,17 @@
 (defun random-position (cursor)
   (setf (cluffer:cursor-position cursor)
 	(random (1+ (cluffer:item-count cursor)))))
+
+(defun random-insertions ()
+  (let ((linea (make-instance 'cluffer-simple-line:line))
+	(lineb (make-instance 'cluffer-standard-line:line))
+	(cursorla (make-instance
+		      'cluffer-simple-line:detached-left-sticky-cursor))
+	(cursorra (make-instance
+		      'cluffer-simple-line:detached-right-sticky-cursor))
+	(cursorlb (make-instance
+		      'cluffer-standard-line:detached-left-sticky-cursor))
+	(cursorrr (make-instance
+		      'cluffer-standard-line:detached-right-sticky-cursor)))
+    nil))
+
