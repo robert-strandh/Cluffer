71b0ce5dcd92d769453ae96010c0bb3227d03aad
Robert Strandh <robert.strandh@gmail.com>
2015-12-14 17:31:12 +0100

    New function for doing a large number of random deletions.

--- Test/test-standard-line.lisp
+++ Test/test-standard-line.lisp
@@ -43,5 +43,43 @@
   (loop repeat 100
 	do (random-insertions-iteration)))
 
+(defun random-deletions-iteration ()
+  (let ((linea (make-instance 'cluffer-simple-line:line))
+	(lineb (make-instance 'cluffer-standard-line:closed-line))
+	(cursorla (make-instance
+		      'cluffer-simple-line:detached-left-sticky-cursor))
+	(cursorra (make-instance
+		      'cluffer-simple-line:detached-right-sticky-cursor))
+	(cursorlb (make-instance
+		      'cluffer-standard-line:detached-left-sticky-cursor))
+	(cursorrb (make-instance
+		      'cluffer-standard-line:detached-right-sticky-cursor)))
+    (cluffer:attach-cursor cursorla linea)
+    (cluffer:attach-cursor cursorra linea)
+    (cluffer:attach-cursor cursorlb lineb)
+    (cluffer:attach-cursor cursorrb lineb)
+    (loop repeat 1000
+	  for item = (random 100000)
+	  do (cluffer:insert-item cursorla item)
+	     (cluffer:insert-item cursorlb item))
+    (loop for size from 1000 above 0
+	  for pos1 = (random size)
+	  for pos2 = (random size)
+	  do (setf (cluffer:cursor-position cursorla) pos1)
+	     (setf (cluffer:cursor-position cursorlb) pos1)
+	     (setf (cluffer:cursor-position cursorra) pos2)
+	     (setf (cluffer:cursor-position cursorrb) pos2)
+	     (if (zerop (random 2))
+		 (progn (cluffer:delete-item cursorla)
+			(cluffer:delete-item cursorlb))
+		 (progn (cluffer:delete-item cursorra)
+			(cluffer:delete-item cursorrb)))
+	     (assert (= (cluffer:cursor-position cursorla)
+			(cluffer:cursor-position cursorlb)))
+	     (assert (= (cluffer:cursor-position cursorra)
+			(cluffer:cursor-position cursorrb)))
+	     (assert (equalp (cluffer:items cursorla)
+			     (cluffer:items cursorlb))))))
+
 (defun test-standard-line ()
   (random-insertions))
