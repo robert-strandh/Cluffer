ece7007b56e920709fc5e59a8b9822bd0ea6b063
Robert Strandh <robert.strandh@gmail.com>
2015-12-16 05:11:37 +0100

    Add comment explaining what the internal SPLIT-LINE does.

--- Base/internal-protocol.lisp
+++ Base/internal-protocol.lisp
@@ -1,5 +1,8 @@
 (cl:in-package #:cluffer-internal)
 
+;;; This generic function removes all the items to the right of the
+;;; cursor in the line in which the cursor is located before the call,
+;;; and returns a second line in which those items have been inserted.
 (defgeneric split-line (cursor))
 
 (defgeneric join-line (line1 line2))
