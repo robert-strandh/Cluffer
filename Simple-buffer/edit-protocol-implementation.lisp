3d8674635360295b3112b415493d114726ea39e4
Robert Strandh <robert.strandh@gmail.com>
2015-12-16 08:57:15 +0100

    Define method on ITEM-COUNT specialized to the simple BUFFER.

--- Simple-buffer/edit-protocol-implementation.lisp
+++ Simple-buffer/edit-protocol-implementation.lisp
@@ -11,3 +11,10 @@
 (defmethod cluffer:line-count ((buffer buffer))
   (length (contents buffer)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Method on generic function ITEM-COUNT.
+
+(defmethod cluffer:item-count ((buffer buffer))
+  (loop for node across (contents buffer)
+	sum (cluffer:item-count (line node))))
