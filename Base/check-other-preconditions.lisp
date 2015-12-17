8dd3ba8e72e670ba0442056d96bb91fd30d97cf1
Robert Strandh <robert.strandh@gmail.com>
2015-12-17 08:08:58 +0100

    Move :BEFORE method for checking precondition to new component for that purpose.

--- Base/check-other-preconditions.lisp
+++ Base/check-other-preconditions.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:cluffer-base)
+
+(defmethod cluffer:find-line :before ((buffer cluffer:buffer) line-number)
+  (when (minusp line-number)
+    (error 'cluffer:beginning-of-buffer))
+  (when (>= line-number (cluffer:line-count buffer))
+    (error 'cluffer:end-of-buffer)))
--- Base/methods.lisp
+++ Base/methods.lisp
@@ -90,12 +90,6 @@
 	 (dock (cluffer-internal:dock line)))
     (cluffer-internal:notify-item-count-changed dock -1)))
 
-(defmethod cluffer:find-line :before ((buffer cluffer:buffer) line-number)
-  (when (minusp line-number)
-    (error 'cluffer:beginning-of-buffer))
-  (when (>= line-number (cluffer:line-count buffer))
-    (error 'cluffer:end-of-buffer)))
-
 ;;; Default method on NOTIFY-ITEM-COUNT-CHANGED.  This method does
 ;;; nothing, and is used when a line is not attached to a buffer.
 (defmethod cluffer-internal:notify-item-count-changed ((doc null) delta)
