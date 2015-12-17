801873e940bf3285f31cd2ed62281c0b79a6d140
Robert Strandh <robert.strandh@gmail.com>
2015-12-17 08:11:17 +0100

    Define :BEFORE method on INSERT-ITEM-AT-POSITION checking that POSITION is valid.

--- Base/check-other-preconditions.lisp
+++ Base/check-other-preconditions.lisp
@@ -1,5 +1,12 @@
 (cl:in-package #:cluffer-base)
 
+(defmethod cluffer:insert-item-at-position :before
+    ((line cluffer:line) item position)
+  (when (minusp position)
+    (error 'cluffer:beginning-of-line))
+  (when (> position (cluffer:item-count line))
+    (error 'cluffer:end-of-buffer)))
+
 (defmethod cluffer:find-line :before ((buffer cluffer:buffer) line-number)
   (when (minusp line-number)
     (error 'cluffer:beginning-of-buffer))
