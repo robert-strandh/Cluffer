b1a31e018de42ccca2c19d0200e20632a90d7714
Robert Strandh <robert.strandh@gmail.com>
2015-12-20 08:27:22 +0100

    Change the signature of the internal generic function SPLIT-LINE.

--- Base/internal-protocol.lisp
+++ Base/internal-protocol.lisp
@@ -1,9 +1,18 @@
 (cl:in-package #:cluffer-internal)
 
-;;; This generic function removes all the items to the right of the
-;;; cursor in the line in which the cursor is located before the call,
-;;; and returns a second line in which those items have been inserted.
-(defgeneric split-line (cursor))
+;;; This generic function removes all the items to the right of
+;;; POSITION in LINE, and returns a second line in which those items
+;;; have been inserted.  Cursors that are located at positions
+;;; strictly greater than POSITION are moved to the new line.  Cursors
+;;; that are located at positions strictly less than POSITION remain
+;;; in their old positions.  What happens to cursors located at
+;;; POSITION depends on the exact type of the line and the exact type
+;;; of those cursors.  The STANDARD-LINE implementation provides two
+;;; types of cursors, namely left-sticky and right-sticky cursors.
+;;; For this implementation, left-sticky cursors located at POSITION
+;;; remain in LINE and right-sticky cursors located at POSITION are
+;;; moved to the beginning of the new line.
+(defgeneric split-line (line position))
 
 ;;; This generic function attaches all of the items of the second line
 ;;; to the end of the first line.
