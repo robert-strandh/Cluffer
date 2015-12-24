8d464bfa954e1a6a74a3c683fd37d07e089ae947
Robert Strandh <robert.strandh@gmail.com>
2015-12-24 16:33:34 +0100

    Define method on BUFFER-SPLIT-LINE for simple buffer.

--- Simple-buffer/edit-protocol-implementation.lisp
+++ Simple-buffer/edit-protocol-implementation.lisp
@@ -33,3 +33,30 @@
 (defmethod cluffer-internal:buffer-line-number
     ((buffer buffer) (dock node) line)
   (position line (contents buffer) :test #'eq :key #'line))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Method on internal generic function BUFFER-SPLIT-LINE.
+
+(defmethod cluffer-internal:buffer-split-line
+    ((buffer buffer) (dock node) (line cluffer:line) position)
+  (let* ((line-number (cluffer-internal:buffer-line-number buffer dock line))
+	 (contents (contents buffer))
+	 (new-contents (make-array (1+ (length contents)))))
+    (replace new-contents contents :start1 0
+				   :start2 0
+				   :end1 (1+ line-number))
+    (replace new-contents contents :start1 (+ line-number 2)
+				   :start2 (1+ line-number))
+    (setf (modify-time dock) (incf (cluffer:current-time buffer)))
+    (let* ((new-line (cluffer-internal:split-line line position))
+	   (time (incf (cluffer:current-time buffer)))
+	   (new-node (make-instance 'node
+		       :buffer buffer
+		       :create-time time
+		       :modify-time time
+		       :line new-line)))
+      (setf (cluffer-internal:dock new-line) new-node)
+      (setf (aref new-contents (1+ line-number)) new-node))
+    (setf (contents buffer) new-contents))
+  nil)
