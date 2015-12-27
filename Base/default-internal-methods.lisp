f6655e10b40a1c5ee84704dcc3f2dccb56196cbd
Robert Strandh <robert.strandh@gmail.com>
2015-12-27 05:57:41 +0100

    Add method on DOCK-JOIN-LINE specialized to a NULL dock.

--- Base/default-internal-methods.lisp
+++ Base/default-internal-methods.lisp
@@ -12,3 +12,6 @@
 
 (defmethod dock-split-line ((dock dock) (line cluffer:line) position)
   (buffer-split-line (buffer dock) dock line position))
+
+(defmethod dock-join-line ((dock null) (line cluffer:line))
+  (error 'cluffer:line-detached))
