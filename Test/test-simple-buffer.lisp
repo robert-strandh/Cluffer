2d84201c100a72671ea0c8f0efe81ee0b6172982
Robert Strandh <robert.strandh@gmail.com>
2015-12-29 06:28:15 +0100

    Add test for verifying that FIND-LINE finds the initial line of a simple buffer.

--- Test/test-simple-buffer.lisp
+++ Test/test-simple-buffer.lisp
@@ -7,5 +7,12 @@
     (assert (= (cluffer:line-count buffer) 1))
     (assert (= (cluffer:item-count buffer) 0))))
 
+(defun test-simple-buffer-2 ()
+  (let* ((line (make-instance 'cluffer-simple-line:line))
+	 (buffer (make-instance 'cluffer-simple-buffer:buffer
+		   :initial-line line)))
+    (assert (eq line (cluffer:find-line buffer 0)))))
+
 (defun test-simple-buffer ()
-  (test-simple-buffer-1))
+  (test-simple-buffer-1)
+  (test-simple-buffer-2))
