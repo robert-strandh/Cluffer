8f8776c27d7292bef50dc5a3f8ed29ce276fe617
Robert Strandh <robert.strandh@gmail.com>
2015-12-29 05:57:47 +0100

    Define macro for testing forms that signal an error.

--- Test/utilities.lisp
+++ Test/utilities.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:cluffer-test)
+
+(defmacro assert-error (form error-type)
+  `(multiple-value-bind (value error)
+       (ignore-errors ,form)
+     (declare (ignore value))
+     (assert (typep error ',error-type))))
