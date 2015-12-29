736678b189cf60c386403af79c8db2c2109f1dca
Robert Strandh <robert.strandh@gmail.com>
2015-12-29 06:17:50 +0100

    Add test for checking that a newly created buffer contains a single line.

--- Test/test-simple-buffer.lisp
+++ Test/test-simple-buffer.lisp
@@ -1,4 +1,10 @@
 (cl:in-package #:cluffer-test)
 
+(defun test-simple-buffer-1 ()
+  (let* ((line (make-instance 'cluffer-simple-line:line))
+	 (buffer (make-instance 'cluffer-simple-buffer:buffer
+		   :initial-line line)))
+    (assert (= (cluffer:line-count buffer) 1))))
+
 (defun test-simple-buffer ()
-  )
+  (test-simple-buffer-1))
