f663c13f2e6af11cc985788a4892320507e5d305
Robert Strandh <robert.strandh@gmail.com>
2015-12-29 04:51:46 +0100

    Check error when attempting to split a line beyond the end.

--- Test/test-simple-line.lisp
+++ Test/test-simple-line.lisp
@@ -229,7 +229,11 @@
     (multiple-value-bind (value error)
 	(ignore-errors (cluffer:split-line-at-position line -1))
       (declare (ignore value))
-      (assert (typep error 'cluffer:beginning-of-line)))))
+      (assert (typep error 'cluffer:beginning-of-line)))
+    (multiple-value-bind (value error)
+	(ignore-errors (cluffer:split-line-at-position line 1))
+      (declare (ignore value))
+      (assert (typep error 'cluffer:end-of-line)))))
 
 (defun test-simple-line ()
   (test-simple-line-1)
