ead2c8a737fac82cad4e430939b5584d691be12f
Robert Strandh <robert.strandh@gmail.com>
2015-12-30 07:58:44 +0100

    Define method on UPDATE specialized to the simple buffer.

--- Simple-buffer/update-protocol-implementation.lisp
+++ Simple-buffer/update-protocol-implementation.lisp
@@ -1,1 +1,43 @@
 (cl:in-package #:cluffer-simple-buffer)
+
+(defmethod cluffer:update
+    ((buffer buffer) time sync skip modify create)
+  (let* ((contents (contents buffer))
+	 (length (length contents))
+	 (i 0)
+	 (skip-count 0))
+    (tagbody
+     skipping
+       (cond ((= i length)
+	      (unless (zerop skip-count)
+		(funcall skip skip-count))
+	      (go out))
+	     ((> (create-time (aref contents i)) time)
+	      (funcall create (line (aref contents i)))
+	      (incf i)
+	      (go not-skipping))
+	     ((> (modify-time (aref contents i)) time)
+	      (funcall modify (line (aref contents i)))
+	      (incf i)
+	      (go not-skipping))
+	     (t
+	      (incf skip-count)
+	      (incf i)
+	      (go skipping)))
+     not-skipping
+       (cond ((= i length)
+	      (go out))
+	     ((> (create-time (aref contents i)) time)
+	      (funcall create (line (aref contents i)))
+	      (incf i)
+	      (go not-skipping))
+	     ((> (modify-time (aref contents i)) time)
+	      (funcall modify (line (aref contents i)))
+	      (incf i)
+	      (go not-skipping))
+	     (t
+	      (funcall sync (line (aref contents i)))
+	      (setf skip-count 0)
+	      (incf i)
+	      (go skipping)))
+     out)))
