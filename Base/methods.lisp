b342abfb07f2b3de9fbf2d0389dfdcf5f56e13d7
Robert Strandh <robert.strandh@gmail.com>
2015-12-31 16:09:11 +0100

    Call NOTIFY-ITEM-COUNT-CHANGED from DELETE method specialized to LINE and not to CURSOR.

--- Base/methods.lisp
+++ Base/methods.lisp
@@ -4,14 +4,16 @@
 ;;; the internal protocol.
 (defmethod cluffer:insert-item-at-position :after
     ((line cluffer:line) item position)
+  (declare (ignore item position))
   (let ((dock (cluffer-internal:dock line)))
     (cluffer-internal:notify-item-count-changed dock 1)))
 
 ;;; This :AFTER method calls the function NOTIFY-ITEM-COUNT-CHANGED in
 ;;; the internal protocol.
-(defmethod cluffer:delete-item :after ((cursor cluffer:cursor))
-  (let* ((line (cluffer:line cursor))
-	 (dock (cluffer-internal:dock line)))
+(defmethod cluffer:delete-item-at-position :after
+    ((line cluffer:line) position)
+  (declare (ignore position))
+  (let ((dock (cluffer-internal:dock line)))
     (cluffer-internal:notify-item-count-changed dock -1)))
 
 ;;; This :AFTER method calls the function NOTIFY-ITEM-COUNT-CHANGED in
