5d67a0b2dc4e714f3e634eb1fda4c723cb59cee7
Robert Strandh <robert.strandh@gmail.com>
2015-12-31 15:17:40 +0100

    Define function for checking that two buffers are probably the same.

--- Test/test-standard-buffer.lisp
+++ Test/test-standard-buffer.lisp
@@ -3,3 +3,11 @@
 (defun check-lines-same (line1 line2)
   (assert (= (cluffer:item-count line1) (cluffer:item-count line2)))
   (assert (equalp (cluffer:items line1) (cluffer:items line1))))
+
+(defun check-buffers-probably-same (buffer1 buffer2)
+  (assert (= (cluffer:line-count buffer1) (cluffer:line-count buffer2)))
+  (assert (= (cluffer:item-count buffer1) (cluffer:item-count buffer2)))
+  (loop for i from 0 below (cluffer:line-count buffer1)
+	when (< (random 1.0) 0.1)
+	  do (check-lines-same (cluffer:find-line buffer1 i)
+			       (cluffer:find-line buffer2 i))))
