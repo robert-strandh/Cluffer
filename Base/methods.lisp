6eefaffb13d18d40e9649231e89cc7003acdfad4
Robert Strandh <robert.strandh@gmail.com>
2015-12-31 16:09:53 +0100

    Remove method on ERASE-ITEM CALLING NOTIFY-ITEM-COUNT-CHANGED.

--- Base/methods.lisp
+++ Base/methods.lisp
@@ -15,10 +15,3 @@
   (declare (ignore position))
   (let ((dock (cluffer-internal:dock line)))
     (cluffer-internal:notify-item-count-changed dock -1)))
-
-;;; This :AFTER method calls the function NOTIFY-ITEM-COUNT-CHANGED in
-;;; the internal protocol.
-(defmethod cluffer:erase-item :after ((cursor cluffer:cursor))
-  (let* ((line (cluffer:line cursor))
-	 (dock (cluffer-internal:dock line)))
-    (cluffer-internal:notify-item-count-changed dock -1)))
