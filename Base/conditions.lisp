d1e5f624f0475a07fd2b7b6cdf3e233276d0fc1d
Robert Strandh <robert.strandh@gmail.com>
2016-12-29 12:54:35 +0100

    Introduce superclass for all Cluffer conditions.

--- Base/conditions.lisp
+++ Base/conditions.lisp
@@ -1,10 +1,13 @@
 (cl:in-package #:cluffer-base)
 
+(define-condition cluffer:cluffer-error (error acclimation:condition)
+  ())
+
 ;;; This condition is signaled when an attempt is made to use a
 ;;; position that is negative, either by moving a cursor there, or by
 ;;; attempting to access an item in such a position.
 (define-condition cluffer:beginning-of-line
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 ;;; This condition is signaled when an attempt is made to use a
@@ -17,29 +20,29 @@
 ;;; number of items in a line, but it is not acceptable to attempt to
 ;;; access an item in a line at that position.
 (define-condition cluffer:end-of-line
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 (define-condition cluffer:beginning-of-buffer
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 (define-condition cluffer:end-of-buffer
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 ;;; This condition is signaled when an attempt is made to use a cursor
 ;;; in an operation that requires that cursor to be detached, but the
 ;;; cursor used in the operation is attached to a line.
 (define-condition cluffer:cursor-attached
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 ;;; This condition is signaled when an attempt is made to use a cursor
 ;;; in an operation that requires that cursor to be attached, but the
 ;;; cursor used in the operation is not attached to any line.
 (define-condition cluffer:cursor-detached
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 ;;; This condition is signaled when an attempt is made to use a line
@@ -49,19 +52,19 @@
 ;;; line number of the line, given that the line number of a line is
 ;;; determined by the buffer to which the line is attached.
 (define-condition cluffer:line-detached
-    (error acclimation:condition)
+    (cluffer-error)
   ())
 
 ;;; This condition is signaled by protocol generic functions that take
 ;;; a line object as an argument, but something other than a line
 ;;; object was given.
 (define-condition cluffer:object-must-be-line
-    (error acclimation:condition)
+    (cluffer-error)
   ((%object :initarg :object :reader object)))
 
 ;;; This condition is signaled by protocol generic functions that take
 ;;; a buffer object as an argument, but something other than a buffer
 ;;; object was given.
 (define-condition cluffer:object-must-be-buffer
-    (error acclimation:condition)
+    (cluffer-error)
   ((%object :initarg :object :reader object)))
